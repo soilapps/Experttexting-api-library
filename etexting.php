54a8d39884b63d3410ab83b0b1985ca96c84f6a3
soilapps <cpagainers@gmail.com>
2016-09-09 21:26:54 +0600

    Create etexting.php

---
+++ etexting.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/**
+ * Config for the Plivo library
+ */
+$config['USERNAME'] = 'cpagainers';
+$config['PASSWORD'] = 'BD!@3456@';
+$config['APIKEY'] = 's7rpfyaryhboxn8';
+$config['END_POINT'] = 'https://www.experttexting.com/exptapi/exptsms.asmx';
